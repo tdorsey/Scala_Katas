3ff65753dc2ffec44ebb9a3d1509fffdf4248377
Tommy Dorsey <tdorsey@users.noreply.github.com>
2018-12-18 17:09:43 -0500

    Remove scala code
    Signed-off-by: tdorsey <tdorsey@paradym.com>

--- src/main/scala/cse/katas/cashregister/Result.scala
+++
@@ -1,9 +0,0 @@
-package cse.katas.cashregister
-
-/**
-  * Created by dnwiebe on 9/13/17.
-  */
-
-case class Result (situation: Situation, cashRegister: CashRegister) {
-
-}
--- src/main/scala/cse/katas/cashregister/Situations.scala
+++
@@ -1,9 +0,0 @@
-package cse.katas.cashregister
-
-/**
-  * Created by dnwiebe on 9/13/17.
-  */
-
-trait Situation
-
-object Success extends Situation
