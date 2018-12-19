773a859eb066f3f389e803cb7f4f62f03c223c79
Tommy Dorsey <tdorsey@users.noreply.github.com>
2018-12-19 17:45:17 -0500

    Implement is_equilateral
    Signed-off-by: tdorsey <tdorsey@paradym.com>

--- src/main/triangle.py
+++ src/main/triangle.py
@@ -1,4 +1,9 @@
-class Triangle():
+class Triangle(object):
+    def __init__(self, side1, side2, side3):
+        self._side1 = side1
+        self._side2 = side2
+        self._side3 = side3
+
 
     @property
     def side1(self):
@@ -8,21 +13,13 @@ class Triangle():
     def side1(self, value):
         self._side1 = value
 
-    @side1.getter
-    def side1(self):
-        return self._side1
-
     @property
     def side2(self):
         return self._side2
 
-    @side1.setter
+    @side2.setter
     def side2(self, value):
         self._side2 = value
-
-    @side1.getter
-    def side2(self):
-        return self._side2
 
     @property
     def side3(self):
@@ -32,22 +29,13 @@ class Triangle():
     def side3(self, value):
         self._side3 = value
 
-    @side3.getter
-    def side3(self):
-        return self._side3
-
-    def __init__(self, side1, side2, side3):
-        self.side1 = side1
-        self.side2 = side2
-        self.side3 = side3
-
     def is_equilateral(self):
-        return True
+        return self.side1 == self.side2 == self.side3
 
     def is_right(self):
         return True
 
     def is_isosceles(self):
-        return False
+        return True
     def is_scalene(self):
-        return False
+        return True
