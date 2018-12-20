3fb448ccc3af1b70b423313443f7de3c64eac9d1
tdorsey <tdorsey@users.noreply.github.com>
2018-12-20 16:59:05 -0500

    Develop (#1)
    Final version

--- src/main/triangle.py
+++ src/main/triangle.py
@@ -1,4 +1,21 @@
-class Triangle():
+from itertools import permutations
+
+
+class Triangle(object):
+    def __init__(self, side1, side2, side3):
+
+        if any([value <= 0 for value in [side1, side2, side3]]):
+            raise ValueError("The length of a Triangle's side must be a positive int")
+
+        self._side1 = side1
+        self._side2 = side2
+        self._side3 = side3
+
+
+
+    @property
+    def sides(self):
+        return [self.side1, self.side2, self.side3]
 
     @property
     def side1(self):
@@ -8,21 +25,13 @@ class Triangle():
     def side1(self, value):
         self._side1 = value
 
-    @side1.getter
-    def side1(self):
-        return self._side1
-
     @property
     def side2(self):
         return self._side2
 
-    @side1.setter
+    @side2.setter
     def side2(self, value):
         self._side2 = value
-
-    @side1.getter
-    def side2(self):
-        return self._side2
 
     @property
     def side3(self):
@@ -32,22 +41,37 @@ class Triangle():
     def side3(self, value):
         self._side3 = value
 
-    @side3.getter
-    def side3(self):
-        return self._side3
-
-    def __init__(self, side1, side2, side3):
-        self.side1 = side1
-        self.side2 = side2
-        self.side3 = side3
-
+    @property
     def is_equilateral(self):
-        return True
+        return self.side1 == self.side2 == self.side3
 
+    @property
     def is_right(self):
+        # Find index of longest side and pop it into c,
+        # sides is a read only property, so make a copy to pop/unpack correctly
+        sides = self.sides
+        max_index = sides.index(max(sides))
+        c = sides.pop(max_index)
+        a,b = sides
+        return a ** 2 + b ** 2 == c ** 2
+
+    def is_valid_triangle(self):
+        # Triangle inequality, sum of any two sides  must be > third
+        # Check each side against all the others. Note that a combination is not the same as a unique permutation
+        # e.g, the combination a = 2, b = 3, c = 5 is incorrect, where permutations include a = 5, b = 3, c = 2
+        # limit calculations to unique permutations only to avoid repeating 3,3,3 and 3,3,3
+        sides = self.sides
+        for p in set(permutations(sides, len(sides))):
+            a,b,c = p
+            if a >= (b + c):
+                return False
         return True
 
+    @property
     def is_isosceles(self):
-        return False
+        return bool([s for s in self.sides if self.sides.count(s) == 2 ])
+
+    @property
     def is_scalene(self):
-        return False
+        return self.is_valid_triangle() and not (self.is_equilateral or self.is_isosceles or self.is_right)
+
--- src/test/test_triangle.py
+++ src/test/test_triangle.py
@@ -1,26 +1,137 @@
+import pytest
 from main.triangle import Triangle
 
-def test_equilateral():
-    t = Triangle(1,2,3)
-    assert(False)
+"""
+[3, 3, 3],  # 1, Equilateral
+[5, 5, 3]  # 2, Isosceles
+[3, 4, 5],  # 3 Right
+[2, 3, 4],  # 4 Scalene
+[2, 3, 5]  # 5 Not a triangle. TODO: Could be colinear, check with customer to clarify
+"""
 
-def test_isosceles():
-    assert(False)
 
-def test_right():
-    return True
+equilateral_data = [
+    ([3, 3, 3], True),
+    ([5, 5, 3], False),
+    ([5, 3, 5], False),
+    ([3, 4, 5], False),
+    ([2, 3, 4], False),
+    ([2, 3, 5], False)
+]
 
-def test_scalene():
-    return True
+isosceles_data = [
+    ([3, 3, 3], False),
+    ([5, 5, 3], True),
+    ([5, 3, 5], True),
+    ([3, 4, 5], False),
+    ([2, 3, 4], False),
+    ([2, 3, 5], False)
+]
 
-def test_invalid_triangle():
-    return True
+right_data = [
+        ([3, 3, 3], False),
+        ([5, 5, 3], False),
+        ([5, 3, 3], False),
+        ([3, 4, 5], True),
+        ([2, 3, 4], False),
+        ([2, 3, 5], False)
+    ]
 
-def test_combination():
-    return True
+scalene_data = [
+    ([3, 3, 3], False),
+    ([5, 5, 3], False),
+    ([5, 3, 5], False),
+    ([3, 4, 5], False),
+    ([2, 3, 4], True),
+    ([2, 3, 5], False)
+]
 
-def test_integer_input():
-    return True
+invalid_triangle_data =  [
+    ([3, 3, 3], True),
+    ([5, 5, 3], True),
+    ([5, 3, 5], True),
+    ([3, 4, 5], True),
+    ([2, 3, 4], True),
+    ([2, 3, 5], False)
+]
 
-def test_noninteger_input():
-    return True
+invalid_args_data = [
+    ([-1, -2, -3], ValueError),
+    ([1, -2, 3], ValueError),
+    ([1, 2, -3], ValueError),
+    ([0, 0, -0], ValueError),
+    ([3, 4, 0], ValueError),
+    ([-12313, 4, 0], ValueError),
+    (["stringdata", 3, -5], TypeError),
+    ([1,2], TypeError),
+    ([1, 2, dict()], TypeError),
+    ([], TypeError)
+]
+
+
+@pytest.fixture(scope='module', params=equilateral_data)
+def equilateral_test_case(request):
+    return request.param
+
+
+@pytest.fixture(scope='module', params=isosceles_data)
+def isosceles_test_case(request):
+    return request.param
+
+
+@pytest.fixture(scope='module', params=right_data)
+def right_test_case(request):
+    return request.param
+
+
+@pytest.fixture(scope='module', params=scalene_data)
+def scalene_test_case(request):
+    return request.param
+
+
+@pytest.fixture(scope='module', params=invalid_triangle_data)
+def invalid_triangle_test_case(request):
+    return request.param
+
+
+@pytest.fixture(scope='module', params=invalid_args_data)
+def invalid_args_test_case(request):
+    return request.param
+
+
+def test_equilateral(equilateral_test_case):
+    lengths, expected = equilateral_test_case
+    triangle = Triangle(*lengths)
+    assert (triangle.is_equilateral == expected)
+
+
+def test_isosceles(isosceles_test_case):
+    lengths, expected = isosceles_test_case
+    triangle = Triangle(*lengths)
+    assert (triangle.is_isosceles == expected)
+
+
+def test_right(right_test_case):
+    lengths, expected = right_test_case
+    triangle = Triangle(*lengths)
+    assert (triangle.is_right == expected)
+
+
+def test_scalene(scalene_test_case):
+    lengths, expected = scalene_test_case
+    triangle = Triangle(*lengths)
+    assert (triangle.is_scalene == expected)
+
+
+def test_invalid_triangle(invalid_triangle_test_case):
+    lengths, expected = invalid_triangle_test_case
+    triangle = Triangle(*lengths)
+
+
+def test_invalid_args(invalid_args_test_case):
+    lengths, expected = invalid_args_test_case
+    with pytest.raises(expected):
+        Triangle(*lengths)
+
+
+
